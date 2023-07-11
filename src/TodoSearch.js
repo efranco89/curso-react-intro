aff994bcf26566841cbf7eccdced13f7a25ee7ae
Enrique Franco <luis.franco@wizeline.com>
2023-07-10 20:31:29 -0500

    basic actions added to TodoSearch and CreateTodo

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,7 +1,18 @@
 import './TodoSearch.css'
 function TodoSearch() {
   return (
-    <input className="TodoSearch" placeholder="Cortar Cebolla"/>
+    <input 
+      className="TodoSearch" 
+      placeholder="Cortar Cebolla"
+      onChange={
+        (event) => {
+          console.log('Escribiste en el TodoSearch');
+          console.log(event);
+          console.log(event.target);
+          console.log(event.target.value);
+        }
+      }
+    />
   )
 }
 
